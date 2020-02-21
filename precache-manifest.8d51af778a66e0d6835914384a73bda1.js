aa1c67093845ce940116226c9457883104c0e1e6
HenriqueMisael <HenriqueMisael@users.noreply.github.com>
2020-02-21 13:38:29 +0000

    deploy: bc6f9de468905af7f671b9dad3a397b4fea14d85

---
+++ precache-manifest.8d51af778a66e0d6835914384a73bda1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93d5c17a45acf4eab6441407184eaf72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d61361923c282654cc73",
+    "url": "/static/css/2.5ea632f0.chunk.css"
+  },
+  {
+    "revision": "d61361923c282654cc73",
+    "url": "/static/js/2.543f2025.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.543f2025.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27e87a6420fc71db224e",
+    "url": "/static/js/main.68055f35.chunk.js"
+  },
+  {
+    "revision": "3ff0c7195d88336245a1",
+    "url": "/static/js/runtime-main.fd0a4265.js"
+  }
+]);
