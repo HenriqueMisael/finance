c61bca201368309958a17161c83f5fa24ff185d8
HenriqueMisael <HenriqueMisael@users.noreply.github.com>
2020-03-07 18:02:37 +0000

    deploy: ae11523ffc3fa7e54fb76e467d209c4bbeba9c5f

--- precache-manifest.91339f806ac12faf944f30502776ef92.js
+++ precache-manifest.91339f806ac12faf944f30502776ef92.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fa9e907cec6e6b939c62b334b322ab2a",
+    "revision": "6ab60c91b84b14f0dba8ec67885962fc",
     "url": "/index.html"
   },
   {
-    "revision": "b8341818d1b2d4bc54fc",
+    "revision": "506dc78162d219ee0525",
     "url": "/static/css/2.5ea632f0.chunk.css"
   },
   {
-    "revision": "d4234f89dbffe34a926d",
+    "revision": "8785d95128374c77704a",
     "url": "/static/css/main.e5de2ec8.chunk.css"
   },
   {
-    "revision": "b8341818d1b2d4bc54fc",
-    "url": "/static/js/2.89634f67.chunk.js"
+    "revision": "506dc78162d219ee0525",
+    "url": "/static/js/2.ca6857c8.chunk.js"
   },
   {
     "revision": "928d7b5eb39d16fe9a880722c974b51e",
-    "url": "/static/js/2.89634f67.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.ca6857c8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d4234f89dbffe34a926d",
-    "url": "/static/js/main.428b3143.chunk.js"
+    "revision": "8785d95128374c77704a",
+    "url": "/static/js/main.e884b195.chunk.js"
   },
   {
     "revision": "3ff0c7195d88336245a1",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.86268668a577602714255b242a34cb52.js"
+  "/precache-manifest.91339f806ac12faf944f30502776ef92.js"
 );
 
 self.addEventListener('message', (event) => {
